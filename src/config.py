2925b8f91977f3f8c412091fcafd9042c6bab5eb
Chad Young <cyoung@twinstrandbio.com>
2024-07-24 11:52:55 -0700

    explore multiple campaigns

--- src/config.py
+++ src/config.py
@@ -23,6 +23,12 @@ episodes_to_remove = [
     {'campaign_no': 1, 'arc_no': 1, 'episode_no': 12}
 ]
 
+# colors to use for campaigns
+campaign_colors = {
+    'background': ['#59627d', '#5d7a5f', '#7d5959'],
+    'color':      ['#1037ad', '#0ca816', '#ab1515']
+}
+
 # colors to use for arcs within campaigns (each sublist is a separate campaign)
 arc_colors = [
     ['#bfb5b2', '#83b58e', '#5e3c58', '#ba9a74', '#2e4045']
